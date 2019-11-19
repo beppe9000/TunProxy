07e8fca84739ca0d58f47b695f79bd40dd5e31d4
beppe9000 <splatter@gmx.com>
2019-11-19 21:49:21 +0100

    fix merge conflict

--- android_app/app/src/main/java/tun/proxy/SimplePreferenceFragment.java
+++ android_app/app/src/main/java/tun/proxy/SimplePreferenceFragment.java
@@ -3,26 +3,28 @@ package tun.proxy;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.*;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.*;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class SimplePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+import static android.preference.Preference.*;
 
+public class SimplePreferenceFragment extends PreferenceFragment
+        implements OnPreferenceClickListener {
     public static final String VPN_CONNECTION_MODE = "vpn_connection_mode";
     public static final String VPN_DISALLOWED_APPLICATION_LIST = "vpn_disallowed_application_list";
     public static final String VPN_ALLOWED_APPLICATION_LIST = "vpn_allowed_application_list";
@@ -34,18 +36,20 @@ public class SimplePreferenceFragment extends PreferenceFragment implements Pref
         setHasOptionsMenu(true);
 
         /* Allowed / Disallowed Application */
-        final ListPreference pkg_selection = (ListPreference) this.findPreference(VPN_CONNECTION_MODE);
-        pkg_selection.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        final ListPreference prefPackage = (ListPreference) this.findPreference(VPN_CONNECTION_MODE);
+        final PreferenceScreen prefDisallow = (PreferenceScreen) findPreference(VPN_DISALLOWED_APPLICATION_LIST);
+        final PreferenceScreen prefAllow = (PreferenceScreen) findPreference(VPN_ALLOWED_APPLICATION_LIST);
+        prefDisallow.setOnPreferenceClickListener(this);
+        prefAllow.setOnPreferenceClickListener(this);
+
+        prefPackage.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object value) {
             if (preference instanceof ListPreference) {
-                ListPreference listPreference = (ListPreference) preference;
+                final ListPreference listPreference = (ListPreference) preference;
                 int index = listPreference.findIndexOfValue((String) value);
-
-                PreferenceScreen disallow = (PreferenceScreen) findPreference(VPN_DISALLOWED_APPLICATION_LIST);
-                PreferenceScreen allow = (PreferenceScreen) findPreference(VPN_ALLOWED_APPLICATION_LIST);
-                disallow.setEnabled(index == 0);
-                allow.setEnabled(index != 0);
+                prefDisallow.setEnabled(index == MyApplication.VPNMode.DISALLOW.ordinal());
+                prefAllow.setEnabled(index == MyApplication.VPNMode.ALLOW.ordinal());
 
                 // Set the summary to reflect the new value.
                 preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
@@ -54,23 +58,19 @@ public class SimplePreferenceFragment extends PreferenceFragment implements Pref
             return true;
             }
         });
-        pkg_selection.setSummary(pkg_selection.getEntry());
-        PreferenceScreen disallow = (PreferenceScreen) findPreference(VPN_DISALLOWED_APPLICATION_LIST);
-        PreferenceScreen allow = (PreferenceScreen) findPreference(VPN_ALLOWED_APPLICATION_LIST);
-        disallow.setEnabled(MyApplication.VPNMode.DISALLOW.name().equals(pkg_selection.getValue()));
-        allow.setEnabled(MyApplication.VPNMode.ALLOW.name().equals(pkg_selection.getValue()));
-
-        findPreference(VPN_DISALLOWED_APPLICATION_LIST).setOnPreferenceClickListener(this);
-        findPreference(VPN_ALLOWED_APPLICATION_LIST).setOnPreferenceClickListener(this);
+        prefPackage.setSummary(prefPackage.getEntry());
+        prefDisallow.setEnabled(MyApplication.VPNMode.DISALLOW.name().equals(prefPackage.getValue()));
+        prefAllow.setEnabled(MyApplication.VPNMode.ALLOW.name().equals(prefPackage.getValue()));
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == android.R.id.home) {
-            startActivity(new Intent(getActivity(), MainActivity.class));
-            return true;
+        switch (id) {
+            case android.R.id.home:
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -100,13 +100,16 @@ public class SimplePreferenceFragment extends PreferenceFragment implements Pref
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class PackageListPreferenceFragment extends PreferenceFragment {
-        private MyApplication.VPNMode mode;
-        private PreferenceScreen mRootPreferenceScreen;
+    public static class PackageListPreferenceFragment extends PreferenceFragment
+            implements SearchView.OnQueryTextListener, SearchView.OnCloseListener {
+        final private Map<String, Boolean> mAllPackageInfoMap = new HashMap<String, Boolean>();
 
+        private MyApplication.VPNMode mode = MyApplication.VPNMode.DISALLOW;
+        private MyApplication.AppSortBy appSortBy = MyApplication.AppSortBy.APPNAME;
+        private PreferenceScreen mFilterPreferenceScreen;
 
         public static PackageListPreferenceFragment newInstance(MyApplication.VPNMode mode) {
-            PackageListPreferenceFragment fragment = new PackageListPreferenceFragment();
+            final PackageListPreferenceFragment fragment = new PackageListPreferenceFragment();
             fragment.mode = mode;
             return fragment;
         }
@@ -115,104 +118,201 @@ public class SimplePreferenceFragment extends PreferenceFragment implements Pref
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setHasOptionsMenu(true);
-            mRootPreferenceScreen = getPreferenceManager().createPreferenceScreen(getActivity());
-            setPreferenceScreen(mRootPreferenceScreen);
+            mFilterPreferenceScreen = getPreferenceManager().createPreferenceScreen(getActivity());
+            setPreferenceScreen(mFilterPreferenceScreen);
+        }
+
+        private String searchFilter = "";
+        private SearchView searchView;
+
+        protected void filter(String filter) {
+            this.filter(filter, this.appSortBy);
+        }
+
+        protected void filter(String filter, final MyApplication.AppSortBy sortBy) {
+            if (filter == null) {
+                filter = searchFilter;
+            } else {
+                searchFilter = filter;
+            }
+            this.appSortBy = sortBy;
+
+            Set<String> selected = this.getAllSelectedPackageSet();
+            storeSelectedPackageSet(selected);
+
+            this.removeAllPreferenceScreen();
+            this.filterPackagesPreferences(filter, sortBy);
         }
 
         @Override
-        public void onPause()  {
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            // Menuの設定
+            inflater.inflate(R.menu.menu_search, menu);
+
+            final MenuItem menuItem = menu.findItem(R.id.menu_search_item);
+
+            this.searchView = (SearchView) menuItem.getActionView();
+            this.searchView.setOnQueryTextListener(this);
+            this.searchView.setOnCloseListener(this);
+            this.searchView.setSubmitButtonEnabled(false);
+
+        }
+
+        @Override
+        public void onPause() {
             super.onPause();
-            storeSelectedPackageSet(this.getSelectedPackageSet());
+            Set<String> selected = this.getAllSelectedPackageSet();
+            storeSelectedPackageSet(selected);
         }
 
         @Override
         public void onResume() {
             super.onResume();
-            removeAllPreferenceScreen();
-            buildPackagesPreferences();
-            loadSelectedPackage();
+            Set<String> loadMap = MyApplication.getInstance().loadVPNApplication(this.mode);
+            for (String pkgName : loadMap) {
+                this.mAllPackageInfoMap.put(pkgName, loadMap.contains(pkgName));
+            }
+            filter(null);
         }
 
         private void removeAllPreferenceScreen() {
-            mRootPreferenceScreen.removeAll();
+            mFilterPreferenceScreen.removeAll();
         }
 
-        private void buildPackagesPreferences() {
-            Context context = MyApplication.getInstance().getApplicationContext();
-            PackageManager pm = context.getPackageManager();
-            List<PackageInfo> installedPackages = pm.getInstalledPackages(PackageManager.GET_META_DATA);
-
-            // {FIX} 13-03-2019: every app of this type forgets to properly sort package names... ― Beppe9000
-            Comparator<PackageInfo> pack_comp = new Comparator<PackageInfo>() {
-                @Override public int compare(PackageInfo p1, PackageInfo p2) {				
-				PackageManager xpm = MyApplication.getInstance().getApplicationContext().getPackageManager();
-                String n1 = p1.applicationInfo.loadLabel(xpm).toString().toLowerCase();
-                String n2 = p2.applicationInfo.loadLabel(xpm).toString().toLowerCase();
-                return n1.compareTo(n2);
+        private void filterPackagesPreferences(String filter, final MyApplication.AppSortBy sortBy) {
+            final Context context = MyApplication.getInstance().getApplicationContext();
+            final PackageManager pm = context.getPackageManager();
+            final List<PackageInfo> installedPackages = pm.getInstalledPackages(PackageManager.GET_META_DATA);
+            Collections.sort(installedPackages, new Comparator<PackageInfo>() {
+                @Override
+                public int compare(PackageInfo o1, PackageInfo o2) {
+                    String t1 = "";
+                    String t2 = "";
+                    switch (sortBy) {
+                        case APPNAME:
+                            t1 = o1.applicationInfo.loadLabel(pm).toString();
+                            t2 = o2.applicationInfo.loadLabel(pm).toString();
+                            break;
+                        case PKGNAME:
+                            t1 = o1.packageName;
+                            t2 = o2.packageName;
+                            break;
+                    }
+                    return t1.compareTo(t2);
                 }
-            };
-             Collections.sort(installedPackages, pack_comp);
-            // {/FIX}
-            
+            });
             for (final PackageInfo pi : installedPackages) {
-                final Preference preference = buildPackagePreferences(pm, pi);
-                mRootPreferenceScreen.addPreference(preference);
+                String t1 = pi.applicationInfo.loadLabel(pm).toString();
+                if (filter.trim().isEmpty() || t1.toLowerCase().contains(filter.toLowerCase())) {
+                    final Preference preference = buildPackagePreferences(pm, pi);
+                    mFilterPreferenceScreen.addPreference(preference);
+                }
             }
         }
 
         private Preference buildPackagePreferences(final PackageManager pm, final PackageInfo pi) {
-            final CheckBoxPreference p = new CheckBoxPreference(getActivity());
-            p.setTitle(pi.applicationInfo.loadLabel(pm).toString());
-            p.setSummary(pi.packageName);
-            return p;
+            final CheckBoxPreference prefCheck = new CheckBoxPreference(getActivity());
+            prefCheck.setIcon(pi.applicationInfo.loadIcon(pm));
+            prefCheck.setTitle(pi.applicationInfo.loadLabel(pm).toString());
+            prefCheck.setSummary(pi.packageName);
+            boolean ckecked = this.mAllPackageInfoMap.containsKey(pi.packageName) ? this.mAllPackageInfoMap.get(pi.packageName) : false;
+            prefCheck.setChecked(ckecked);
+            OnPreferenceClickListener click = new OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    mAllPackageInfoMap.put(prefCheck.getSummary().toString(), prefCheck.isChecked());
+                    return false;
+                }
+            };
+            prefCheck.setOnPreferenceClickListener(click);
+            return prefCheck;
         }
 
-        private Set<String> getSelectedPackageSet() {
-            Set<String> selected = new HashSet<>();
-            for (int i = 0; i < this.mRootPreferenceScreen.getPreferenceCount(); i++) {
-                Preference pref = this.mRootPreferenceScreen.getPreference(i);
+        private Set<String> getFilterSelectedPackageSet() {
+            final Set<String> selected = new HashSet<>();
+            for (int i = 0; i < this.mFilterPreferenceScreen.getPreferenceCount(); i++) {
+                Preference pref = this.mFilterPreferenceScreen.getPreference(i);
                 if ((pref instanceof CheckBoxPreference)) {
-                    CheckBoxPreference pref_check = (CheckBoxPreference) pref;
-                    if (pref_check.isChecked())
-                        selected.add(pref_check.getSummary().toString());
+                    CheckBoxPreference prefCheck = (CheckBoxPreference) pref;
+                    if (prefCheck.isChecked()) {
+                        selected.add(prefCheck.getSummary().toString());
+                    }
                 }
             }
             return selected;
         }
 
         private void setSelectedPackageSet(Set<String> selected) {
-            for (int i = 0; i < this.mRootPreferenceScreen.getPreferenceCount(); i++) {
-                Preference pref = this.mRootPreferenceScreen.getPreference(i);
+            for (int i = 0; i < this.mFilterPreferenceScreen.getPreferenceCount(); i++) {
+                Preference pref = this.mFilterPreferenceScreen.getPreference(i);
                 if ((pref instanceof CheckBoxPreference)) {
-                    CheckBoxPreference pref_check = (CheckBoxPreference) pref;
-                    if (selected.contains(pref_check.getSummary()))
-                        pref_check.setChecked(true);
+                    CheckBoxPreference prefCheck = (CheckBoxPreference) pref;
+                    if (selected.contains((prefCheck.getSummary()))) {
+                        prefCheck.setChecked(true);
+                    }
                 }
             }
         }
 
-        private void loadSelectedPackage() {
-            this.getArguments();
-            mode  = MyApplication.getInstance().loadVPNMode();
-            Set<String> selected = MyApplication.getInstance().loadVPNApplication(mode);
-            setSelectedPackageSet(selected);
+        private Set<String>  getAllSelectedPackageSet() {
+            Set<String> selected = this.getFilterSelectedPackageSet();
+            for (Map.Entry<String, Boolean> value : this.mAllPackageInfoMap.entrySet()) {
+                if (value.getValue()) {
+                    selected.add(value.getKey());
+                }
+            }
+            return selected;
         }
 
         private void storeSelectedPackageSet(final Set<String> set) {
-            MyApplication.getInstance().storeVPNMode(mode);
-            MyApplication.getInstance().storeVPNApplication(mode, set);
+            MyApplication.getInstance().storeVPNMode(this.mode);
+            MyApplication.getInstance().storeVPNApplication(this.mode, set);
         }
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SimplePreferenceActivity.class));
-                return true;
+            switch (id) {
+                case android.R.id.home:
+                    startActivity(new Intent(getActivity(), SimplePreferenceActivity.class));
+                    return true;
+                case R.id.menu_sort_app_name:
+                    item.setChecked(!item.isChecked());
+                    filter(null, MyApplication.AppSortBy.APPNAME);
+                    break;
+                case R.id.menu_sort_pkg_name:
+                    item.setChecked(!item.isChecked());
+                    filter(null, MyApplication.AppSortBy.PKGNAME);
+                    break;
             }
             return super.onOptionsItemSelected(item);
         }
 
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            this.searchView.clearFocus();
+            if (!query.trim().isEmpty()) {
+                filter(query);
+                return true;
+            } else {
+                filter("");
+                return true;
+            }
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            return false;
+        }
+
+        @Override
+        public boolean onClose() {
+            Set<String> selected = this.getAllSelectedPackageSet();
+            storeSelectedPackageSet(selected);
+            filter("");
+            return false;
+        }
     }
 
 }
